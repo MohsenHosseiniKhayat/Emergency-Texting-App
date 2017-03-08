db260015c497aaa4651cffa1e2dd172ac966c9ac
Mohsen Hosseini Khayat <mohsen.hosseini@outlook.com>
2017-03-07 20:05:37 -0800

    Enabled remote configuration via Firebase (Step 11)

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -153,9 +153,26 @@ public class MainActivity extends AppCompatActivity
         mLinearLayoutManager.setStackFromEnd(true);
         mMessageRecyclerView.setLayoutManager(mLinearLayoutManager);
 
-        
+        // Initialize Firebase Remote Config.
+            mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
 
+        // Define Firebase Remote Config Settings.
+            FirebaseRemoteConfigSettings firebaseRemoteConfigSettings =
+                    new FirebaseRemoteConfigSettings.Builder()
+                            .setDeveloperModeEnabled(true)
+                            .build();
 
+        // Define default config values. Defaults are used when fetched config values are not
+        // available. Eg: if an error occurred fetching values from the server.
+            Map<String, Object> defaultConfigMap = new HashMap<>();
+            defaultConfigMap.put("friendly_msg_length", 10L);
+
+        // Apply config settings and default values.
+            mFirebaseRemoteConfig.setConfigSettings(firebaseRemoteConfigSettings);
+            mFirebaseRemoteConfig.setDefaults(defaultConfigMap);
+
+        // Fetch remote config.
+            fetchConfig();
 
 
 
@@ -291,14 +308,14 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId())
-        {
-            //If user selects sign out option
+        switch (item.getItemId()) {
+            case R.id.fresh_config_menu:
+                fetchConfig();
+                return true;
             case R.id.sign_out_menu:
                 mFirebaseAuth.signOut();
-                Auth.GoogleSignInApi.signOut(mGoogleApiClient);
                 mUsername = ANONYMOUS;
-                startActivity(new Intent (this, SignInActivity.class));
+                startActivity(new Intent(this, SignInActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -338,5 +355,50 @@ public class MainActivity extends AppCompatActivity
                 .setObject(friendlyMessage.getName(), MESSAGE_URL.concat(friendlyMessage.getId()))
                 .setMetadata(new Action.Metadata.Builder().setUpload(false))
                 .build();
+    }
+
+    // Fetch the config to determine the allowed length of messages.
+    public void fetchConfig() {
+        long cacheExpiration = 3600; // 1 hour in seconds
+        // If developer mode is enabled reduce cacheExpiration to 0 so that
+        // each fetch goes to the server. This should not be used in release
+        // builds.
+        if (mFirebaseRemoteConfig.getInfo().getConfigSettings()
+                .isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+        mFirebaseRemoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Make the fetched config available via
+                        // FirebaseRemoteConfig get<type> calls.
+                        mFirebaseRemoteConfig.activateFetched();
+                        applyRetrievedLengthLimit();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // There has been an error fetching the config
+                        Log.w(TAG, "Error fetching config: " +
+                                e.getMessage());
+                        applyRetrievedLengthLimit();
+                    }
+                });
+    }
+
+
+    /**
+     * Apply retrieved length limit to edit text field.
+     * This result may be fresh from the server or it may be from cached
+     * values.
+     */
+    private void applyRetrievedLengthLimit() {
+        Long emergency_msg_length =
+                mFirebaseRemoteConfig.getLong("emergency_msg_length");
+        mMessageEditText.setFilters(new InputFilter[]{new
+                InputFilter.LengthFilter(emergency_msg_length.intValue())});
+        Log.d(TAG, "FML is: " + emergency_msg_length);
     }
 }

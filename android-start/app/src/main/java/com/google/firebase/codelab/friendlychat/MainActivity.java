50db9bde1e7ab292d3be7db2084af5272326f2af
MohsenHosseiniKhayat <Mohsen.Hosseini@outlook.com>
2017-05-13 15:15:46 -0700

    Alert functionalities Reinstaed, Visual adjustments made

--- android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ android-start/app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -18,13 +18,18 @@ package com.google.firebase.codelab.friendlychat;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract;
+import android.provider.Settings;
 import android.provider.SyncStateContract;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -116,6 +121,8 @@ public class MainActivity extends AppCompatActivity
     // Firebase instance variables
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
+    LocationListener locListener;
+    LocationManager myManager;
     private DatabaseReference mFirebaseDatabaseReference;
     private FirebaseRecyclerAdapter<FriendlyMessage, MessageViewHolder> mFirebaseAdapter;
     private FirebaseRemoteConfig mFirebaseRemoteConfig;
@@ -317,6 +324,8 @@ public class MainActivity extends AppCompatActivity
                 startActivity(alertIntent);
             }
         });
+
+        createAlertButtons();
     }
 
     @Override
@@ -458,16 +467,165 @@ public class MainActivity extends AppCompatActivity
         startActivityForResult(intent, REQUEST_INVITE);
     }
 
-    private boolean isRegistered ()
-    {
-        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SHARED_PREF,MODE_PRIVATE);
-        return sharedPreferences.getBoolean(Constants.REGISTERED,false);
+    private boolean isRegistered() {
+        SharedPreferences sharedPreferences = getSharedPreferences(Constants.SHARED_PREF, MODE_PRIVATE);
+        return sharedPreferences.getBoolean(Constants.REGISTERED, false);
     }
 
-    private void registerDevice ()
-    {
+    private void registerDevice() {
         //Firebase firebase = new Firebase (Constants.FIREBASE_APP);
     }
 
+
+    public void createAlertButtons() {
+        //Creates a location listener to obtain user location
+        myManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        //Setup the buttons and the user info
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mUsername = mFirebaseUser.getDisplayName();
+        mFirebaseDatabaseReference = FirebaseDatabase.getInstance().getReference();
+        Button accidentButton = (Button) findViewById(R.id.buttonAccidentAlert);
+        Button assaultButton = (Button) findViewById(R.id.buttonAssaultAlert);
+        Button disasterButton = (Button) findViewById(R.id.buttonDisasterAlert);
+        Button safeButton = (Button) findViewById(R.id.buttonSafeAlert);
+        Button callEmergency = (Button) findViewById(R.id.buttonCall);
+
+
+        //Create message status strings
+        final String ACCIDENT = "is involved in an ACCIDENT";
+        final String ASSAULT = "is being Robbed/Assaulted";
+        final String NATURALDISASTER = "is affected by a NATURAL DISASTER";
+        final String SAFE = "is SAFE now";
+        final String EMEGENCYDIAL = "is calling 911";
+        final ArrayList<String> favContacts = new ArrayList<>();
+
+        //Add favorite contacts from Choose Contact Activity
+        SharedPreferences sharedPreferences = getSharedPreferences("MyFavorites", Context.MODE_PRIVATE);
+        String newContactEmail = sharedPreferences.getString("Email Address", "");
+        favContacts.add(newContactEmail);
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        myManager.requestLocationUpdates("gps", 1000, 5, locListener);
+        final MainActivity thisStrong = this;
+
+        //Create all the buttonListeners
+        accidentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Display dialog fragment after success
+
+                String accidentMessage = mUsername + " " + ACCIDENT;
+                try {
+                    FriendlyMessage friendlyMessage = new FriendlyMessage(accidentMessage, mUsername, mPhotoUrl, favContacts, myManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                    mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
+                    Toast.makeText(thisStrong, "Location Sent", Toast.LENGTH_LONG).show();
+                } catch (SecurityException se) {
+                    Log.wtf("SendAlertActivity", "User should already have given permission: " + se.toString());
+                }
+            }
+        });
+
+        assaultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Display dialog fragment after success
+
+                String assaultMessage = mUsername + " " + ASSAULT;
+                try {
+                    FriendlyMessage friendlyMessage = new FriendlyMessage(assaultMessage, mUsername, mPhotoUrl, favContacts, myManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                    mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
+                    Toast.makeText(thisStrong, "Location Sent", Toast.LENGTH_LONG).show();
+                } catch (SecurityException se) {
+                    Log.wtf("SendAlertActivity", "User should already have given permission: " + se.toString());
+                }
+            }
+        });
+
+        disasterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Display dialog fragment after success
+
+                String disasterMessage = mUsername + " " + NATURALDISASTER;
+                try {
+                    FriendlyMessage friendlyMessage = new FriendlyMessage(disasterMessage, mUsername, mPhotoUrl, favContacts, myManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                    mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
+                    Toast.makeText(thisStrong, "Location Sent", Toast.LENGTH_LONG).show();
+                } catch (SecurityException se) {
+                    Log.wtf("SendAlertActivity", "User should already have given permission: " + se.toString());
+                }
+            }
+        });
+
+        safeButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String safetyMessage = mUsername + " " + SAFE;
+                try {
+                    FriendlyMessage friendlyMessage = new FriendlyMessage(safetyMessage, mUsername, mPhotoUrl, favContacts, myManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                    mFirebaseDatabaseReference.child(MESSAGES_CHILD).push().setValue(friendlyMessage);
+                    Toast.makeText(thisStrong, "Safety notice sent!", Toast.LENGTH_LONG).show();
+                } catch (SecurityException se) {
+                    Log.wtf("SendAlertActivity", "User should already have given permission: " + se.toString());
+                }
+            }
+        });
+
+        callEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneIntent = new Intent(Intent.ACTION_CALL);
+                phoneIntent.setData(Uri.parse("tel:912"));
+
+
+                if (ActivityCompat.checkSelfPermission(thisStrong, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(phoneIntent);
+            }
+        });
+    }
 
 }
